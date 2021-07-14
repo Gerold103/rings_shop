85882eb25761257bbb83e1f3fcb2800aa598b83d
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2021-07-14 10:05:49 +0200

    Update frontend from the designer
    * Improved scrolling;
    * New location for the preorder button;
    * Smaller picter sizes.

--- script.js
+++ script.js
@@ -1,46 +1,76 @@
 const animationDuration = 200;
 const images = Array.from(document.getElementsByClassName('photo'));
+const carousel = document.getElementsByClassName('carousel')[0];
+
+function animate(start, finish, fps, duration, callback){
+    const count = parseInt(fps * duration / 1000);
+    if (finish === start)
+        return Promise.resolve(finish);
+    return new Promise((resolve) => {
+        const step = (finish - start)/(count - 1);
+        let value = start;
+        callback(value);
+        let interval = setInterval(()=>{
+            value += step;
+            if ((finish > start && value > finish - step/2) || 
+                (finish < start && value < finish - step/2))
+            {
+                callback(finish);
+                clearInterval(interval);
+                resolve(finish);
+            }
+            else {
+                callback(value);
+            }
+        }, duration/count);
+    });
+}
+
 const cards = images.map((img, index) => {
     return {
         shiftX: 0,
+        _shiftX: 0,
         baseX: index * 100,
         img: img,
         animate: function(){
+            return new Promise((resolve) => {
+                animate(this._shiftX, this.shiftX, 60, animationDuration, (v)=>{
+                    this.img.style.transform = 'translateX('+parseInt(v)+'%)';
+                }).then((v)=>{
+                    this._shiftX = v;
+                    resolve();
+                });
+            });
+        },
+
+        noAnimate: function(){
             this.img.style.transform = 'translateX('+this.shiftX+'%)';
         },
-        noAnimate: function(callback){
-            const self = this;
-            const transition = this.img.style.transition;
-            this.img.style.transition = 'all 0.001s linear';
-            this.img.style.opacity = 0;
-            callback();
-            self.animate();
 
-            setTimeout(function(){
-                self.img.style.opacity = 1;
-                self.img.style.transition = transition;
-            }, animationDuration);
-        },
         fixLeft: function() {
             if (this.actualShiftX() <= -100) {
-                this.noAnimate(() => {
-                    this.shiftX += 400;
-                });
+                this.shiftX += 400;
+                this._shiftX += 400;
+                this.noAnimate();
             }
         },
+
         fixRight: function() {
             if (this.actualShiftX() > 300) {
-                this.noAnimate(() => {
-                    this.shiftX -= 400;
-                });
+                this.shiftX -= 400;
+                this._shiftX -= 400;
+                this.noAnimate();
             }
         },
+
         apply: function(){
-            this.img.style.zIndex = parseInt((this.actualShiftX()+300)/100);
             this.fixRight();
-            this.animate();
-            setTimeout(() => this.fixLeft(), animationDuration);
+            this.img.style.zIndex = parseInt((this.actualShiftX()+300)/100);
+            this.animate().then(()=>{
+                this.fixLeft();
+            });
         },
+
         actualShiftX: function(){
             return this.baseX + this.shiftX;
         }
@@ -51,29 +81,53 @@ let disabled = false;
 
 cards.forEach(card => {
     card.img.onclick = function(e){
-        if (!disabled){
-            let step = -card.actualShiftX();
-            if (step < -100)
-                step = -100;
-            else if (step > 100)
-                step = 100;
-            move(step);
-            disabled = true;
-            setTimeout(()=>{disabled = false;}, animationDuration + 100);
-        }
+        let step = -card.actualShiftX();
+        move(step);
     }
 });
 
 
-function move(step){
-    let left, right;
-    cards.forEach(c => {
-        c.shiftX += step;
-        c.apply();
-        if (!left || c.actualShiftX() < left.actualShiftX())
-            left = c;
+carousel.onwheel = (e)=>{
+    move(-e.deltaY);
+};
 
-        if (!right || c.actualShiftX() > right.actualShiftX())
-            right = c;
+function move(step){
+    if (step > 0){
+        step = 100;
+    }
+    if (step < 0){
+        step = -100;
+    }
+    return new Promise((resolve) => {
+        if (!disabled && step){
+            disabled = true;
+            cards.forEach(c => {
+                c.shiftX += step;
+                c.apply();
+            });
+            setTimeout(()=>{
+                disabled = false;
+                resolve();
+            }, animationDuration + 100);
+        }
     });
 }
+
+/***********************************/
+let moving = null;
+carousel.addEventListener('touchstart', (e) => {
+    if (moving === null){
+        moving = e.touches[0].clientX;
+    }
+});
+carousel.addEventListener('touchmove', (e) => {
+    const x = e.touches[0].clientX;
+    console.log(moving, x);
+    if (moving && x != moving){
+        const step = x - moving;
+        moving = undefined;
+        move(step).then(()=>{
+            moving = null;
+        });
+    }
+});
